f2391647faf6be063c6cc4a335546f9d0e994bb7
Chris McClelland <prophet3636@gmail.com>
2020-10-31 15:11:09 +0000

    Produce report

--- build.sh
+++ build.sh
@@ -16,3 +16,7 @@ fi
 cmake --build build --target install
 cd build
 ctest
+wget https://raw.githubusercontent.com/adarmalik/gtest2html/master/gtest2html.xslt
+for i in $(find build/ -name "*-tests.xml"); do
+  xsltproc gtest2html.xslt $i > ${i%%.*}.html
+done

0245913d14152b9f92138239e324b9e7201132e2
Chris McClelland <prophet3636@gmail.com>
2020-10-31 16:12:45 +0000

    Config: Debug

--- build.cmd
+++ build.cmd
@@ -1,8 +1,8 @@
 @echo off
 md build
 cd build
-cmake -DBUILD_TESTING=1 -DCMAKE_BUILD_TYPE=Debug -Dgtest_force_shared_crt=ON -A x64 ..
+cmake -DBUILD_TESTING=1 -Dgtest_force_shared_crt=ON -A x64 ..
 cd ..
-cmake --build build
+cmake --build build --config Debug
 cd build
-ctest -D Debug
+ctest -C Debug
